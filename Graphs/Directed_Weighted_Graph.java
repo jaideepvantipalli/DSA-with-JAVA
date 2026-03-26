cf36adcb0b1a9bca6f5a36674f7791af283b6722
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-03-26 19:36:50 +0530

    graphs problems started

---
+++ Graphs/Directed_Weighted_Graph.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Directed_Weighted_Graph {
+    int n;
+    int M[][];
+    public Directed_Weighted_Graph(int n) {
+        this.n=n;
+        M=new int[n][n];
+    }
+    void addEdge(int u,int v,int w){
+        M[u][v]=w;
+    }
+    void printgraph(){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(M[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no of Nodes : ");
+        int n=sc.nextInt();
+        Directed_Weighted_Graph obj=new Directed_Weighted_Graph(n);
+        while(true){
+            System.out.print("Enter 1 for exit ,2 for add edge : ");
+            int choice=sc.nextInt();
+            if(choice==1){
+                break;
+            }
+            if(choice==2){
+                    System.out.print("Enter edge (u v ,weight : w) : ");
+                    int u=sc.nextInt();
+                    int v=sc.nextInt();
+                    int w=sc.nextInt();
+                    obj.addEdge(u,v,w);
+                }
+        }
+        obj.printgraph();
+        sc.close();
+    }
+}
---
+++ Graphs/UndirectedGraph.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class UndirectedGraph {
+    int n;
+    int M[][];
+    public UndirectedGraph(int n) {
+        this.n=n;
+        M=new int[n][n];
+    }
+    void addEdge(int u,int v){
+        M[u][v]=1;
+        M[v][u]=1;
+    }
+    void printgraph(){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(M[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no of Nodes : ");
+        int n=sc.nextInt();
+        UndirectedGraph obj=new UndirectedGraph(n);
+        while(true){
+            System.out.print("Enter 1 for exit ,2 for add edge : ");
+            int choice=sc.nextInt();
+            if(choice==1){
+                break;
+            }
+            if(choice==2){
+                    System.out.print("Enter edge (u v) : ");
+                    int u=sc.nextInt();
+                    int v=sc.nextInt();
+                    obj.addEdge(u,v);
+                }
+            }
+        obj.printgraph();
+        sc.close();
+    }
+}
